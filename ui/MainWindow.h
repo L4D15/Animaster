487b8f223d0efa8f2aac1fa773f3cdfc2c03fa72
José Ladislao Lainez Ortega <thedragonladis@gmail.com>
2013-12-26 19:02:05 +0100

    Implemented various ui tools.

--- ui/MainWindow.h
+++ ui/MainWindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "src/DiceBag.h"
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,23 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_diceRollerInput_returnPressed();
+
+    void on_attackValue_valueChanged(int arg1);
+
+    void on_defenseValue_valueChanged(int arg1);
+
+    void on_armorValue_valueChanged(int arg1);
+
+    void on_damageValue_valueChanged(int arg1);
+
+private:
+    void            calculateAttack();
+
 private:
     Ui::MainWindow *ui;
+    DiceBag*        diceBag;
 };
 
 #endif // MAINWINDOW_H
